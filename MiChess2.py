2c7ed8f807b506333e98d142ad3597b039a130ac
Mykhailo Poliakov <stone4onelove@gmail.com>
2026-02-24 02:03:54 +0100

    added controll system

--- MiChess2.py
+++ MiChess2.py
@@ -28,7 +28,7 @@ def resource_path(relative_path):
 textures = {
     'dot' : pygame.image.load(resource_path("textures/dot.png")).convert_alpha(),
     # board
-    'board' : pygame.image.load(resource_path("textures/board.jpg")).convert_alpha(),
+    'board' : pygame.image.load(resource_path("textures/board.png")).convert_alpha(),
     'pieces' : {
         # white pieces
         'wp' : pygame.image.load(resource_path("textures/wp.png")).convert_alpha(),
@@ -46,32 +46,36 @@ textures = {
         'bq' : pygame.image.load(resource_path("textures/bq.png")).convert_alpha(),
     }
 }
-textures['board'] = pygame.transform.scale(textures['board'], (1080, 1080))
+textures['board'] = pygame.transform.scale(textures['board'], ( 960, 960))
 textures['dot'] = pygame.transform.scale(textures['dot'], (10, 10))
-textures['mini_board'] = pygame.transform.scale(textures['board'], (220, 220))
+textures['mini_board'] = pygame.transform.scale(textures['board'], (200, 200))
 textures['mini_board'].set_alpha(90)
 
 
-""" Functions """
+""" Create empties """
 def create_dict():
-    double_dict = {}
-    empty_dict = {}
-    all_pos = []
+    _double_dict = {}
+    _empty_dict = {}
+    _all_pos = []
     for i in range(1, 9):
         for j in reversed(range(1, 9)):
-            all_pos.append(str(i) + str(j))
-            empty_dict[str(i) + str(j)] = '  '
-    for square in all_pos:
-        double_dict['dict' + square] = empty_dict
-    return all_pos, empty_dict, double_dict
+            _all_pos.append(str(i) + str(j))
+            _empty_dict[str(i) + str(j)] = '  '
+    for square in _all_pos:
+        _double_dict['dict' + square] = _empty_dict
+    return _all_pos, _empty_dict, _double_dict
+
+all_pos, empty_dict, double_dict = create_dict()
+
+
+""" Functions """
+pass
 
 
 """ Classes """
 class GameState:
-    all_pos, empty_dict, double_dict = create_dict()
     def __init__(self):
         # start values
-        self.mode = "game"
         self.check = False
 
         # switches
@@ -97,15 +101,15 @@ class GameState:
         self.el_passant = [False,[]]
 
         # dictionaries
-        self.comb_possible_moves = copy.deepcopy(self.empty_dict)
-        self.comb_threat_moves = copy.deepcopy(self.empty_dict)
-        self.comb_cover_moves = copy.deepcopy(self.empty_dict)
+        self.comb_possible_moves = copy.deepcopy(empty_dict)
+        self.comb_threat_moves = copy.deepcopy(empty_dict)
+        self.comb_cover_moves = copy.deepcopy(empty_dict)
 
-        self.cover_moves = copy.deepcopy(self.double_dict)
-        self.possible_moves = copy.deepcopy(self.double_dict)
-        self.op_possible_moves = copy.deepcopy(self.double_dict)
-        self.threat_moves = copy.deepcopy(self.double_dict)
-        self.op_threat_moves = copy.deepcopy(self.empty_dict)
+        self.cover_moves = copy.deepcopy(double_dict)
+        self.possible_moves = copy.deepcopy(double_dict)
+        self.op_possible_moves = copy.deepcopy(double_dict)
+        self.threat_moves = copy.deepcopy(double_dict)
+        self.op_threat_moves = copy.deepcopy(empty_dict)
         self.board = {  # main board
             '18': "br", '28': "bh", '38': "bb", '48': "bq", '58': "bk", '68': "bb", '78': "bh", '88': "br",
             '17': "bp", '27': "bp", '37': "bp", '47': "bp", '57': "bp", '67': "bp", '77': "bp", '87': "bp",
@@ -155,14 +159,14 @@ class GameState:
         list(self.board.values()).count('br') == 0 and list(self.board.values()).count('bq') == 0 and
         list(self.board.values()).count('bp') == 0 and list(self.board.values()).count('wr') == 0 and
         list(self.board.values()).count('wq') == 0 and list(self.board.values()).count('wp') == 0):
-            self.mode = "draw"
+            sett.mode = "draw"
         """ Stalemate check """
         if not self.check:
             for move in self.possible_moves:
                 for value in self.possible_moves[move].values():
                     if value == 'x ':
                         return
-            self.mode = "stalemate"
+            sett.mode = "stalemate"
 
     def moves_checker(self, input_board, threats_mode=False):
         """checks all the possible player moves"""
@@ -171,26 +175,26 @@ class GameState:
             direction = 1 if input_board[place][0] == 'w' else - 1
             # if checking threats
             if threats_mode:
-                if str(int(place) + direction - 10) in self.all_pos:
+                if str(int(place) + direction - 10) in all_pos:
                     output_board['dict' + place][str(int(place) + direction - 10)] = 'x '
-                if str(int(place) + direction + 10) in self.all_pos:
+                if str(int(place) + direction + 10) in all_pos:
                     output_board['dict' + place][str(int(place) + direction + 10)] = 'x '
                 return
             # 1 step forward
-            if str(int(place) + direction) in self.all_pos and input_board[str(int(place) + direction)] == '  ':
+            if str(int(place) + direction) in all_pos and input_board[str(int(place) + direction)] == '  ':
                 output_board['dict' + place][str(int(place) + direction)] = 'x '
             # 2 steps forward
-            if str(int(place) + direction) in self.all_pos and str(int(place) + (direction * 2)) in self.all_pos and \
+            if str(int(place) + direction) in all_pos and str(int(place) + (direction * 2)) in all_pos and \
                     input_board[str(int(place) + direction)] == '  ' and (
             input_board[str(int(place) + (direction * 2))]) == '  ' \
                     and ((self.player == 'w' and place[1] == '2') or (self.player == 'b' and place[1] == '7')):
                 output_board['dict' + place][str(int(place) + (direction * 2))] = 'x '
             # capture left piece
-            if str(int(place) + direction - 10) in self.all_pos and input_board[
+            if str(int(place) + direction - 10) in all_pos and input_board[
                 str(int(place) + direction - 10)] in self.opponent_pieces:
                 output_board['dict' + place][str(int(place) + direction - 10)] = 'x '
             # capture right piece
-            if str(int(place) + direction + 10) in self.all_pos and input_board[
+            if str(int(place) + direction + 10) in all_pos and input_board[
                 str(int(place) + direction + 10)] in self.opponent_pieces:
                 output_board['dict' + place][str(int(place) + direction + 10)] = 'x '
             # el passant
@@ -206,7 +210,7 @@ class GameState:
             '''
 
         def horse_move_checker(number):
-            if str((int(place) + number)) in self.all_pos:
+            if str((int(place) + number)) in all_pos:
                 if input_board[str(int(place) + number)] not in self.player_pieces:
                     output_board['dict' + place][str(int(place) + number)] = 'x '
 
@@ -248,7 +252,7 @@ class GameState:
                 output_board['dict' + place][number] = 'x '
 
         def king_move_checker(number):
-            if str((int(place) + number)) in self.all_pos:
+            if str((int(place) + number)) in all_pos:
                 if threats_mode:
                     output_board['dict' + place][str(int(place) + number)] = 'x '
                     return
@@ -267,8 +271,8 @@ class GameState:
         # big_op_king_move_dict = empty_dict.copy()
 
         # cleaning the dictionary
-        output_board = copy.deepcopy(self.double_dict)
-        solo_output_board = copy.deepcopy(self.empty_dict)
+        output_board = copy.deepcopy(double_dict)
+        solo_output_board = copy.deepcopy(empty_dict)
 
         # all possible moves calculation
         for place in input_board.keys():
@@ -483,6 +487,7 @@ class GameState:
 
 class PlayerSettings:
     def __init__(self):
+        self.mode = "game"
         self.f3 = True
         self.sound = True
 
@@ -491,6 +496,21 @@ class PlayerSettings:
         self.choose_side = None
 
 
+
+class Input:
+    def __init__(self):
+        self.start_pos  = ''
+        self.end_pos    = ''
+        self.action = ()
+        # board buttons
+        self.buttons = {}
+        for place in all_pos:
+            x = 480; y = 1020 ; coef = 120
+            self.buttons[place] = pygame.Rect(x + (int(place[0]) - 1) * coef, y - int(place[1]) * coef, coef, coef)
+
+
+
+inp = Input()
 sett = PlayerSettings()
 state = GameState()
 
@@ -498,16 +518,21 @@ state = GameState()
 state.player_change('w')
 state.dict_update()
 
-action = ()
+
 running = True
 while running:
 
     """ BRAIN """
-    if action:
-        state.movement(*action)
-        action = ()
+    if sett.mode == "game":
+        if inp.action:
+            state.movement(*inp.action)
+            inp.action = ()
 
     """ INPUT """
+
+    keys = pygame.key.get_pressed()
+    mouse_keys = pygame.mouse.get_pressed()
+    mouse_pos = pygame.mouse.get_pos()
 
     for event in pygame.event.get():
         # ways to exit
@@ -516,17 +541,26 @@ while running:
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
 
+        # pressing f3
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_F3:
             sett.f3 = not sett.f3
 
-
-        # testing
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_g:
-            action = ('52', '13')
-
-        # testing
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_t:
-            action = ('87', '86')
+        # mouse left button press
+        if sett.mode == "game":
+            # button hold
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                for button in inp.buttons:
+                    if inp.buttons[button].collidepoint(mouse_pos):
+                        inp.start_pos = button
+                        break
+            # button let go
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and inp.start_pos:
+                for button in inp.buttons:
+                    if inp.buttons[button].collidepoint(mouse_pos):
+                        inp.end_pos = button
+                        inp.action = (inp.start_pos, inp.end_pos)
+                        break
+                inp.end_pos = '' ; inp.start_pos = ''
 
 
 
@@ -537,39 +571,39 @@ while running:
     screen.fill(background_color)
 
     # if game is in process
-    if state.mode == "game":
+    if sett.mode == "game":
         # board
-        screen.blit(textures['board'], (420, 0))
+        screen.blit(textures['board'], (480, 60))
 
         # pieces output
         for key in state.board:
             if state.board[key] != "  ":
-                screen.blit(textures['pieces'][state.board[key]], (460 + (int(key[0]) - 1) * 125, 905 - (int(key[1]) - 1) * 125))
+                screen.blit(textures['pieces'][state.board[key]], (480 + (int(key[0]) - 1) * 120, 905 - (int(key[1]) - 1) * 120))
 
 
     # if F3 pressed
     if sett.f3:
 
         # possible moves output
-        screen.blit(textures['mini_board'], ( 0, 55))
+        screen.blit(textures['mini_board'], ( 15, 60))
         text = mini_font.render("possible moves output", True, (255, 255, 255))
-        screen.blit(text, (0, 50))
+        screen.blit(text, (15, 40))
         for key in state.comb_possible_moves:
             if state.comb_possible_moves[key] != '  ':
                 screen.blit(textures['dot'], (20 + (int(key[0]) - 1) * 25, 245 - (int(key[1]) - 1) * 25))
 
         # opponent threats output
-        screen.blit(textures['mini_board'], (0, 285))
+        screen.blit(textures['mini_board'], (15, 290))
         text = mini_font.render("opponent threats output", True, (255, 255, 255))
-        screen.blit(text, (0, 280))
+        screen.blit(text, (15, 270))
         for key in state.comb_threat_moves:
             if state.comb_threat_moves[key] != '  ':
                 screen.blit(textures['dot'], (20 + (int(key[0]) - 1) * 25, 475 - (int(key[1]) - 1) * 25))
 
         # player cover output
-        screen.blit(textures['mini_board'], (0, 515))
+        screen.blit(textures['mini_board'], (15, 520))
         text = mini_font.render("player cover output", True, (255, 255, 255))
-        screen.blit(text, (0, 510))
+        screen.blit(text, (15, 500))
         for key in state.comb_cover_moves:
             if state.comb_cover_moves[key] != '  ':
                 screen.blit(textures['dot'], (20 + (int(key[0]) - 1) * 25, 705 - (int(key[1]) - 1) * 25))
@@ -577,11 +611,10 @@ while running:
         # dop info
         fps = int(clock.get_fps())
         messages = (
-
-        f'Fps : {fps}',
-        f'Check : {state.check}',
-        f'Mode : {state.mode}')
-
+            f'Fps : {fps}',
+            f'Check : {state.check}',
+            f'Mode : {sett.mode}',
+            f'action : {inp.start_pos}, {inp.end_pos}',)
         for num, message in enumerate(messages):
             text = font.render(message, True, (255, 255, 255))
             screen.blit(text, (15, 900 + num * 35))
